5491fc761489ce73b4ba834df5091af4e0304209
baahkusi <nanakbaahkusi@gmail.com>
2024-10-24 18:29:15 +0000

    create multi topics

--- scripts/create_multi_topics.ts
+++ scripts/create_multi_topics.ts
@@ -5,37 +5,11 @@ async function main() {
     "MultiTopicRegistry",
     process.env.MULTI_TOPIC_REGISTRY!
   );
-
-  // await shadow.createTopic(
-  //   "Football Outcome Events",
-  //   "Outcome of football match; home, away, win, home-away, home-draw, away-draw",
-  //   process.env.FOOTBALL_OUTCOME_EVALUATOR!
-  // ); // id 0
-  // await shadow.createTopic(
-  //   "Football Over/Under Events",
-  //   "Whether total goals will be over or under a given value. Values are 1.5, 2.5, 3.5 and 4.5",
-  //   process.env.FOOTBALL_OVER_UNDER_EVALUATOR!
-  // ); // id 1
-  // await shadow.createTopic(
-  //   "Football Correct Score Events",
-  //   "The correct score of a football match",
-  //   process.env.FOOTBALL_CORRECT_SCORE_EVALUATOR!
-  // ); // 2
-  // await shadow.createTopic(
-  //   "Asset Price Bounded",
-  //   "Whether asset price will fall within a given bound",
-  //   process.env.ASSET_PRICE_BOUNDED_EVALUATOR!
-  // ); // 3
-  // await shadow.createTopic(
-  //   "Asset Price Target",
-  //   "Whether asset price be above or below a given amount",
-  //   process.env.ASSET_PRICE_TARGET_EVALUATOR!
-  // ); // 4
   await shadow.createTopic(
-    "Multi General Statements",
-    "Predict outcome of any and all statements.",
-    process.env.MULTI_GENERAL_STATEMENT_EVALUATOR!
-  ); // 5
+    "Football Score Correct Score",
+    "Predict if outcome of football match will be one of the correct scores",
+    process.env.MULTI_FOOTBALL_SCORE_CORRECT_SCORE_EVALUATOR!
+  );
 
   console.log("Topics Created ...");
 }
